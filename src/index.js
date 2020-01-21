5cb8e182df8566d4bae3cc8c16ea31ff2200849e
Alexis Couvreur <alexiscouvreur.pro@gmail.com>
2020-01-21 19:12:19 +0100

    Logging on connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const geohash = require("ngeohash");
-const axios = require("axios");
-const Influx = require("influx");
+const geohash = require('ngeohash');
+const axios = require('axios');
+const Influx = require('influx');
 
 // TCP handles
 const net = require('net');
@@ -9,46 +9,59 @@ const host = '127.0.0.1';
 
 const server = net.createServer();
 server.listen(port, host, () => {
-    console.log('TCP Server is running on port ' + port + '.');
+	console.log('TCP Server is running on port ' + port + '.');
 });
 
 // InfluxDB Initialization.
 const influx = new Influx.InfluxDB({
-    host: process.env.INFLUX_URL,
-    database: process.env.INFLUX_DB
+	host: process.env.INFLUX_URL,
+	database: process.env.INFLUX_DB
 });
 
-server.on('connection', function(sock) {
+let sockets = [];
 
-    sock.on('data', function(data) {
-        //console.log(data);
-        let message = JSON.parse("" + data)
-        // API Initialization.
-        const instance = axios.create({
-            baseURL: "http://ip-api.com/json/"
-        });
-        instance
-            .get(`/${message.ip}`)
-            .then(function(response) {
-                const apiResponse = response.data;
-                influx.writePoints(
-                    [{
-                        measurement: "geossh",
-                        fields: {
-                            value: 1
-                        },
-                        tags: {
-                            geohash: geohash.encode(apiResponse.lat, apiResponse.lon),
-                            username: message.username,
-                            port: message.port,
-                            ip: message.ip
-                        }
-                    }]
-                );
-                console.log("Intruder added")
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
-    });
+server.on('connection', function(sock) {
+	console.log('CONNECTED: ' + sock.remoteAddress + ':' + sock.remotePort);
+	sockets.push(sock);
+
+	sock.on('data', function(data) {
+		//console.log(data);
+		let message = JSON.parse('' + data);
+		// API Initialization.
+		const instance = axios.create({
+			baseURL: 'http://ip-api.com/json/'
+		});
+		instance
+			.get(`/${message.ip}`)
+			.then(function(response) {
+				const apiResponse = response.data;
+				influx.writePoints([
+					{
+						measurement: 'geossh',
+						fields: {
+							value: 1
+						},
+						tags: {
+							geohash: geohash.encode(apiResponse.lat, apiResponse.lon),
+							username: message.username,
+							port: message.port,
+							ip: message.ip
+						}
+					}
+				]);
+				console.log('Intruder added');
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	});
+
+	// Add a 'close' event handler to this instance of socket
+	sock.on('close', function(data) {
+		let index = sockets.findIndex(function(o) {
+			return o.remoteAddress === sock.remoteAddress && o.remotePort === sock.remotePort;
+		});
+		if (index !== -1) sockets.splice(index, 1);
+		console.log('CLOSED: ' + sock.remoteAddress + ' ' + sock.remotePort);
+	});
 });
